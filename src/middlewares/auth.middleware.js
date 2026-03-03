7e1c40549353fd24ed5fa4f63e5fbb12095e7949
Garv Verma <GarvVermaa@users.noreply.github.com>
2026-03-03 23:57:44 +0530

    fixed Jwt import

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,4 @@
-import { jwt } from "jsonwebtoken";
+import  jwt  from "jsonwebtoken";
 import { User } from "../models/user.models.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
